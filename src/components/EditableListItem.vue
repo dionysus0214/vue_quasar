48cad3435279c5f8cfaa9e8a4c626452ac5cb8f6
dionysus0214 <hiba0214@naver.com>
2021-12-16 14:32:23 +0900

    tutorial project layout setting

---
+++ src/components/EditableListItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-item class="editable-list-item q-pa-none">
+    <q-item-section>
+
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+// import {
+//   ref, watch, onMounted, nextTick,
+// } from 'vue';
+// import { useQuasar } from 'quasar';
+
+/* eslint-disable */
+export default {
+  name: 'EditableListItem',
+  props: {
+  },
+  setup() {
+    return {
+    };
+  },
+};
+</script>
---
+++ src/pages/Project.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-page>
+    <div class="popup-page-content-container column q-col-gutter-md">
+    </div>
+  </q-page>
+</template>
+
+<script>
+/* eslint-disable no-unused-vars */
+import { ref, onBeforeMount, computed } from 'vue';
+import { scroll } from 'quasar';
+import { siteList, templateList, orderDataArray } from '../constants/data';
+
+import SSelect from '../components/SSelect.vue';
+import EditableListItem from '../components/EditableListItem.vue';
+
+const TAP_VALUE_MAP = {
+  auto: 0,
+  manual: 1,
+};
+
+export default {
+  components: {
+    SSelect,
+    EditableListItem,
+  },
+  setup() {
+    return {
+    };
+  },
+};
+</script>
+
+<style>
+.detail-column {
+  width: 500px;
+}
+.guide-column {
+  width: 400px;
+}
+</style>
