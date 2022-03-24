d0349a608dc4b51236ed03f8a783e623632cf170
dionysus0214 <hiba0214@naver.com>
2022-03-24 15:02:46 +0900

    SPagination lastPage, rowPerPage 속성 추가

--- src/components/SPagination.vue
+++ src/components/SPagination.vue
@@ -1,23 +1,92 @@
 <template>
-  <q-pagination
-    rounded
-    flat
-    unelevated
-    color="grey-4"
-    class="s-pagination q-pa-none"
-    active-color="white"
-    direction-links
-    boundary-links
-    :ripple="false"
-    :boundary-numbers="false"
-    :icon-prev="paginationLeftIcon"
-    :icon-next="paginationRightIcon"
-    :icon-first="paginationFirstIcon"
-    :icon-last="paginationLastIcon"
-  />
+  <div class="pagination row">
+    <div class="icons-container" v-if="paginationInfo.startPage !== 1">
+      <q-btn
+        flat
+        dense
+        round
+        :ripple="false"
+        unelevated
+        type="button"
+        align="center"
+        class="bg-transparent first-btn"
+        text-color="grey-4"
+        :icon="paginationFirstIcon"
+        @click="updateModelValue('goFirst')"
+      />
+      <q-btn
+        flat
+        dense
+        round
+        :ripple="false"
+        unelevated
+        type="button"
+        align="center"
+        class="left-btn"
+        text-color="grey-4"
+        :icon="paginationLeftIcon"
+        @click="updateModelValue('goLeft')"
+      />
+    </div>
+    <div class="pagination-container">
+      <q-btn
+        v-for="(p, i) in 10"
+        :key="i"
+        v-show="paginationInfo.startPage + i <= paginationInfo.lastPage"
+        flat
+        dense
+        rounded
+        no-wrap
+        unelevated
+        :ripple="false"
+        :label="paginationInfo.startPage + i"
+        text-color="grey-4"
+        type="button"
+        align="center"
+        @click="updateModelValue(paginationInfo.startPage + i)"
+        class="pagination-btn"
+        :class="{
+          'pagination-active-btn':
+            paginationInfo.startPage + i === paginationInfo.currentPage,
+        }"
+      />
+    </div>
+    <div
+      class="icons-container"
+      v-if="paginationInfo.lastPage - paginationInfo.startPage > 10"
+    >
+      <q-btn
+        flat
+        dense
+        round
+        :ripple="false"
+        unelevated
+        type="button"
+        align="center"
+        class="right-btn"
+        text-color="grey-4"
+        :icon="paginationRightIcon"
+        @click="updateModelValue('goRight')"
+      />
+      <q-btn
+        flat
+        dense
+        round
+        :ripple="false"
+        unelevated
+        type="button"
+        align="center"
+        class="last-btn"
+        text-color="grey-4"
+        :icon="paginationLastIcon"
+        @click="updateModelValue('goLast')"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import { ref, watch } from 'vue';
 import {
   paginationLeftIcon,
   paginationRightIcon,
@@ -28,83 +97,166 @@ import {
 export default ({
   name: 'SPagination',
   props: {
-    useDisable: {
-      type: Boolean,
-      default: false,
+    modelValue: {
+      type: Number,
+      default: 1,
+    },
+    lastPage: {
+      type: Number,
+      default: 1,
+      required: true,
+    },
+    rowPerPage: {
+      type: Number,
+      default: 5,
     },
   },
-  setup() {
+  setup(props, { emit }) {
+    const paginationInfo = ref({
+      lastPage: props.lastPage,
+      currentPage: props.modelValue,
+      rowPerPage: props.rowPerPage,
+      startPage: Math.floor((props.modelValue - 1) / 10) * 10 + 1,
+    });
+
+    function updateModelValue(val) {
+      if (val === 'goFirst') {
+        paginationInfo.value.startPage = 1;
+        paginationInfo.value.currentPage = 1;
+      } else if (val === 'goLeft' && paginationInfo.value.startPage - 10 > 0) {
+        paginationInfo.value.startPage -= 10;
+        paginationInfo.value.currentPage = paginationInfo.value.startPage + 9;
+      } else if (
+        val === 'goRight'
+        && paginationInfo.value.startPage + 10 < paginationInfo.value.lastPage
+      ) {
+        paginationInfo.value.startPage += 10;
+        paginationInfo.value.currentPage = paginationInfo.value.startPage;
+      } else if (val === 'goLast') {
+        if (paginationInfo.value.lastPage % 10 !== 0) {
+          paginationInfo.value.startPage = Math.floor(paginationInfo.value.lastPage / 10) * 10 + 1;
+        } else {
+          paginationInfo.value.startPage = (Math.floor(paginationInfo.value.lastPage / 10) - 1) * 10 + 1;
+        }
+        paginationInfo.value.currentPage = paginationInfo.value.lastPage;
+      } else {
+        paginationInfo.value.currentPage = val;
+      }
+      emit('update:modelValue', paginationInfo.value.currentPage);
+    }
+
+    watch(
+      () => props.modelValue,
+      (newVal) => {
+        paginationInfo.value.currentPage = newVal;
+        paginationInfo.value.startPage = Math.floor((newVal - 1) / 10) * 10 + 1;
+      },
+    );
+
+    watch(
+      () => props.lastPage,
+      (newVal) => {
+        paginationInfo.value.lastPage = newVal;
+      },
+    );
+
     return {
       paginationRightIcon,
       paginationLeftIcon,
       paginationLastIcon,
       paginationFirstIcon,
+
+      paginationInfo,
+
+      updateModelValue,
     };
   },
 });
 </script>
 
 <style lang="scss">
-.s-pagination {
-  & > .q-btn-item {
-    & > .q-btn__content {
-      padding: 4px 10px !important;
-      & > svg {
-        width: 12px;
-        height: 12px;
-      }
-    }
-    &.disabled {
-      display: none;
-    }
-  }
-  & > div {
-    & > .q-btn {
-      & > .q-btn__content {
-        padding: 2px 10px 4px 10px !important;
-      }
-      // active
-      &.text-white {
-        background: $indigo-7;
-      }
-    }
-  }
-  // common style
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   .q-btn {
+    min-height: 28px;
+    height: 28px;
     margin: 0 4px;
-    min: {
-      width: 28px !important;
-      height: 28px !important;
-    };
-    height: 28px !important;
-    padding: 0 !important;
+    background: none !important;
     justify-content: center;
     &:hover {
-      &::before {
+      &:before {
         border: 1px solid $indigo-7;
       }
+      .q-focus-helper {
+        opacity: 0 !important;
+        &:before,
+        &:after {
+          opacity: 0 !important;
+        }
+      }
+      .q-btn__content {
+        .q-icon {
+          color: $indigo-7;
+        }
+        > span {
+          color: $indigo-7;
+        }
+      }
     }
   }
-  .disabled {
-    background: none !important;
+  .icons-container {
+    .q-btn {
+      min-width: 28px;
+      width: 28px;
+      &__content {
+        .q-icon {
+          width: 12px;
+          height: 12px;
+        }
+      }
+    }
+  }
+  .pagination-container {
+    .q-btn {
+      min-width: 28px;
+      width: fit-content;
+      padding: 0;
+      &__content {
+        padding: 2px 10px 4px;
+        > span {
+          height: 22px;
+          line-height: 22px;
+          font-size: 14px;
+          font-weight: 400;
+        }
+      }
+    }
+    .pagination-active-btn {
+      &:before {
+        background: $indigo-7 !important;
+      }
+      .q-btn__content {
+        > span {
+          color: white !important;
+        }
+      }
+    }
+  }
+  .q-btn.disabled {
     &:hover {
-      &::before {
+      &:before {
         border: none;
       }
-    }
-    .q-btn__content {
-      .q-icon {
-        color: $grey-6;
+      .q-btn__content {
+        .q-icon {
+          color: $grey-6;
+        }
+        > span {
+          color: $grey-6;
+        }
       }
-      > span {
-        color: $grey-6;
-      }
-    }
-  }
-  .q-focusable:focus,
-  .q-hoverable:hover {
-    & > .q-focus-helper {
-      display: none;
     }
   }
 }
--- src/pages/Table.vue
+++ src/pages/Table.vue
@@ -4,101 +4,41 @@
       :columns="columns"
       :rows="rows"
       row-key="barcode_no"
-      useCustom
+      :useSelect="true"
+      selection="multiple"
+      v-model:selected="selected"
+      v-model:pagination="paginationObj"
       resizable
-      dynamicSlotName="RFID발급할수량"
+      :loading="loading"
     >
       <template v-slot:noData>
-        <div class="full-width text-center">
-          파일을 업로드하세요.
-        </div>
-      </template>
-      <template v-slot:header>
-        <tr>
-          <th v-for="col in columns" :key="col.name">
-            {{ col.label }}
-          </th>
-        </tr>
+        <div class="full-width text-center">파일을 업로드하세요.</div>
       </template>
       <template v-slot:body-cell-product_code="props">
         <q-td>
           <q-chip>{{ props.row.barcode_no }}</q-chip>
         </q-td>
       </template>
+      <template v-slot:body-cell-상품명="props">
+        <q-td style="max-width: 200px">
+          {{ props.value }}
+        </q-td>
+      </template>
     </s-table>
-
-    <br/>
-
-    <s-table
-      :columns="columns"
-      :rows="rows"
-      row-key="barcode_no"
-      :useSelect="true"
-      selection="multiple"
-      v-model:selected="selected"
-    >
-    </s-table>
-
-    <br/>
-
-    <s-markup-table
-      :separator="separator"
-      header="top"
-      class="q-ma-md">
-      <thead>
-        <tr>
-          <th v-for="col in columns" :key="col.name">
-            {{ col.label }}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(r, i) in rows" :key="i">
-          <td class="text-center">{{ r.barcode_no }}</td>
-          <td class="text-center">{{ r.product_code }}</td>
-          <td class="text-center">{{ r.supplier_name }}</td>
-          <td class="text-left">{{ r.공급처상품명 }}</td>
-          <td class="text-left">{{ r.상품명 }}</td>
-          <td class="text-center">{{ r.바코드번호 }}</td>
-          <td class="text-center">{{ r.발주수량 }}</td>
-          <td class="text-center">{{ r.RFID발급할수량 }}</td>
-        </tr>
-      </tbody>
-    </s-markup-table>
-
-    <br/>
-
-    <s-markup-table
-      :separator="separator"
-      header="left"
-      class="q-ma-md">
-      <thead>
-        <tr>
-          <th v-for="col in columns" :key="col.name">
-            {{ col.label }}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(r, i) in rows" :key="i">
-          <td class="text-center">{{ r.barcode_no }}</td>
-          <td class="text-center">{{ r.product_code }}</td>
-          <td class="text-center">{{ r.supplier_name }}</td>
-          <td class="text-left">{{ r.공급처상품명 }}</td>
-          <td class="text-left">{{ r.상품명 }}</td>
-          <td class="text-center">{{ r.바코드번호 }}</td>
-          <td class="text-center">{{ r.발주수량 }}</td>
-          <td class="text-center">{{ r.RFID발급할수량 }}</td>
-        </tr>
-      </tbody>
-    </s-markup-table>
+    <s-pagination
+      v-if="rows.length"
+      v-model="paginationObj.page"
+      :lastPage="paginationObj.lastPage"
+      @update:modelValue="updateModelValue"
+    />
+    <br />
   </div>
 </template>
 
 <script>
-import { ref, watch } from 'vue';
+import { ref } from 'vue';
 import STable from "../components/STable.vue";
-import SMarkupTable from "../components/SMarkupTable.vue";
+import SPagination from "../components/SPagination.vue";
 
 const columns = [
   {
@@ -122,21 +62,24 @@ const columns = [
     format: (v) => (!v ? '-' : v),
   },
   {
-    name: '공급처상품명',
-    label: '공급처상품명',
-    field: '공급처상품명',
+    name: '상품명',
+    label: '상품명',
+    field: '상품명',
     align: 'center',
     format: (v) => (!v ? '-' : v),
-
   },
   {
-    name: '상품명', label: '상품명', field: '상품명', align: 'center', format: (v) => (!v ? '-' : v),
+    name: '옵션명',
+    label: '옵션명',
+    field: '옵션명',
+    align: 'center',
+    format: (v) => (!v ? '-' : v),
   },
   {
-    name: '옵션명', label: '옵션명', field: '옵션명', align: 'center', format: (v) => (!v ? '-' : v),
-  },
-  {
-    name: '발주수량', label: '발주수량', field: '발주수량', align: 'center',
+    name: '발주수량',
+    label: '발주수량',
+    field: '발주수량',
+    align: 'center',
   },
   {
     name: 'RFID발급할수량',
@@ -148,46 +91,203 @@ const columns = [
 
 const rows = [
   {
-    barcode_no: 'M87017K2C', product_code: 'M87017K2C', supplier_name: 'UPTOWNHOLIC', 공급처상품명: '[소가죽] 베이 스웨이드 롱 부츠 (*2color)', 상품명: 'UPTOWNHOLIC Before She Broke Your', 옵션명: '베이지, 235', 발주수량: 2000, RFID발급할수량: 2000,
+    barcode_no: 'M87017K2C',
+    product_code: 'M87017K2C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Yourzzzzzzzzzzzzzz',
+    옵션명: '베이지, 235',
+    발주수량: 2000,
+    RFID발급할수량: 2000,
   },
   {
-    barcode_no: 'M87017K1C', product_code: 'M87017K1C', supplier_name: 'UPTOWNHOLIC', 공급처상품명: '[소가죽] 베이 스웨이드 롱 부츠 (*2color)', 상품명: 'UPTOWNHOLIC Before She Broke Your', 옵션명: '베이지, 230', 발주수량: 100, RFID발급할수량: 100,
+    barcode_no: 'M87017K1C',
+    product_code: 'M87017K1C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '베이지, 230',
+    발주수량: 100,
+    RFID발급할수량: 100,
   },
   {
-    barcode_no: 'M86873K6C', product_code: 'M86873K6C', supplier_name: 'UPTOWNHOLIC', 공급처상품명: '[소가죽] 베이 스웨이드 롱 부츠 (*2color)', 상품명: 'UPTOWNHOLIC Before She Broke Your', 옵션명: '브라운,M', 발주수량: 600, RFID발급할수량: 600,
+    barcode_no: 'M86873K6C',
+    product_code: 'M86873K6C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '브라운,M',
+    발주수량: 600,
+    RFID발급할수량: 600,
   },
   {
-    barcode_no: 'M86873K5C', product_code: 'M86873K5C', supplier_name: 'UPTOWNHOLIC', 공급처상품명: '[소가죽] 베이 스웨이드 롱 부츠 (*2color)', 상품명: 'UPTOWNHOLIC Before She Broke Your', 옵션명: '브라운,S', 발주수량: 200, RFID발급할수량: 200,
+    barcode_no: 'M86873K5C',
+    product_code: 'M86873K5C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '브라운,S',
+    발주수량: 200,
+    RFID발급할수량: 200,
   },
   {
-    barcode_no: '28443420004', product_code: '28443420004', supplier_name: 'ZOOC(상설)', 공급처상품명: 'Z203MSD025', 상품명: '도트 프린트 스커트_Z203MSD025', 옵션명: 'DARK NAVY(DN1) ／ 66', 발주수량: 50, RFID발급할수량: 50,
+    barcode_no: '28443420004',
+    product_code: '28443420004',
+    supplier_name: 'ZOOC(상설)',
+    상품명: '도트 프린트 스커트_Z203MSD025',
+    옵션명: 'DARK NAVY(DN1) ／ 66',
+    발주수량: 50,
+    RFID발급할수량: 50,
   },
   {
-    barcode_no: '28443420003', product_code: '28443420003', supplier_name: 'ZOOC(상설)', 공급처상품명: 'Z203MSD025', 상품명: '도트 프린트 스커트_Z203MSD025', 옵션명: 'DARK NAVY(DN1) ／ 55', 발주수량: 50, RFID발급할수량: 50,
+    barcode_no: '28443420003',
+    product_code: '28443420003',
+    supplier_name: 'ZOOC(상설)',
+    상품명: '도트 프린트 스커트_Z203MSD025',
+    옵션명: 'DARK NAVY(DN1) ／ 55',
+    발주수량: 50,
+    RFID발급할수량: 50,
   },
   {
-    barcode_no: '28442920004', product_code: '28442920004', supplier_name: 'ZOOC(상설)', 공급처상품명: 'Z203MSD025', 상품명: '도트 프린트 스커트_Z203MSD025', 옵션명: 'BLACK(BK1) ／ 66', 발주수량: 50, RFID발급할수량: 50,
+    barcode_no: '28442920004',
+    product_code: '28442920004',
+    supplier_name: 'ZOOC(상설)',
+    상품명: '도트 프린트 스커트_Z203MSD025',
+    옵션명: 'BLACK(BK1) ／ 66',
+    발주수량: 50,
+    RFID발급할수량: 50,
+  },
+  {
+    barcode_no: 'M87017K2Cwe',
+    product_code: 'M87017K2C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '베이지, 235',
+    발주수량: 2000,
+    RFID발급할수량: 2000,
+  },
+  {
+    barcode_no: 'M8701werf7K2C',
+    product_code: 'M87017K2C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '베이지, 235',
+    발주수량: 2000,
+    RFID발급할수량: 2000,
+  },
+  {
+    barcode_no: 'M8701dfg7K2C',
+    product_code: 'M87017K2C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '베이지, 235',
+    발주수량: 2000,
+    RFID발급할수량: 2000,
+  },
+  {
+    barcode_no: 'M8701wertgh7K2C',
+    product_code: 'M87017K2C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '베이지, 235',
+    발주수량: 2000,
+    RFID발급할수량: 2000,
+  },
+  {
+    barcode_no: 'M8703dfg417K2C',
+    product_code: 'M87017K2C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '베이지, 235',
+    발주수량: 2000,
+    RFID발급할수량: 2000,
+  },
+  {
+    barcode_no: 'M8701df23g7K2C',
+    product_code: 'M87017K2C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '베이지, 235',
+    발주수량: 2000,
+    RFID발급할수량: 2000,
+  },
+  {
+    barcode_no: 'M870we1437K2C',
+    product_code: 'M87017K2C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '베이지, 235',
+    발주수량: 2000,
+    RFID발급할수량: 2000,
+  },
+  {
+    barcode_no: 'M870123r67K2C',
+    product_code: 'M87017K2C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '베이지, 235',
+    발주수량: 2000,
+    RFID발급할수량: 2000,
+  },
+  {
+    barcode_no: 'M8706717K2C',
+    product_code: 'M87017K2C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '베이지, 235',
+    발주수량: 2000,
+    RFID발급할수량: 2000,
+  },
+  {
+    barcode_no: 'M878462017K2C',
+    product_code: 'M87017K2C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '베이지, 235',
+    발주수량: 2000,
+    RFID발급할수량: 2000,
+  },
+  {
+    barcode_no: 'M87023417K2C',
+    product_code: 'M87017K2C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '베이지, 235',
+    발주수량: 2000,
+    RFID발급할수량: 2000,
+  },
+  {
+    barcode_no: 'M87017K2C',
+    product_code: 'M87017K2C',
+    supplier_name: 'UPTOWNHOLIC',
+    상품명: 'UPTOWNHOLIC Before She Broke Your',
+    옵션명: '베이지, 235',
+    발주수량: 2000,
+    RFID발급할수량: 2000,
   },
 ];
 
 export default {
   components: {
     STable,
-    SMarkupTable,
+    SPagination,
   },
   setup() {
     const selected = ref([]);
+    const paginationObj = ref({
+      page: 1,
+      rowsPerPage: 50,
+      lastPage: Math.ceil(rows.length / 5),
+    });
 
-    watch(selected,
-      (newVal) => {
-        console.log(newVal);
-      });
-
+    function updateModelValue(current) {
+      paginationObj.value.currentPage = current;
+    }
+    const loading = ref(false);
     return {
       columns,
       rows,
+      loading,
+
       selected,
-      separator: ref('cell'),
+      paginationObj,
+      updateModelValue,
     };
   },
 };
