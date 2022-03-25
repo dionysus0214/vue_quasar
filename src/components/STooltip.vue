b3347a8ea4683336113d6d9f7b6531a8f8159e22
dionysus0214 <hiba0214@naver.com>
2022-03-25 09:00:03 +0900

    STooltip anchor, offset 속성 추가

--- src/components/STooltip.vue
+++ src/components/STooltip.vue
@@ -1,25 +1,137 @@
 <template>
-  <q-tooltip
-    class="bg-indigo-4 s-tooltip"
+  <q-btn
+    no-caps
+    no-wrap
+    dense
+    unelevated
+    :ripple="false"
+    class="s-tooltip-btn"
+    :class="{'no-hover': noHover}"
   >
-    <slot></slot>
-  </q-tooltip>
+    <q-tooltip
+      :anchor="$props.boxAnchor"
+      :self="$props.boxSelf"
+      :offset="$props.boxOffset"
+      class="bg-indigo-4 text-white s-tooltip"
+      :modelValue="$props.modelValue"
+    >
+      <slot></slot>
+    </q-tooltip>
+    <q-tooltip
+      :anchor="$props.arrowAnchor"
+      :self="$props.arrowSelf"
+      :offset="$props.arrowOffset"
+      :class="$props.arrowStyle"
+      class="tooltip-triangle"
+      :modelValue="$props.modelValue"
+    ></q-tooltip>
+  </q-btn>
 </template>
 
 <script>
 export default {
   props: {
-    content: String,
+    modelValue: {
+      type: Boolean,
+      default: false,
+    },
+    noHover: {
+      type: Boolean,
+      default: false,
+    },
+    boxAnchor: {
+      type: String,
+      required: true,
+    },
+    boxSelf: {
+      type: String,
+      required: true,
+    },
+    boxOffset: {
+      type: Array,
+      required: true,
+    },
+    arrowAnchor: {
+      type: String,
+      required: true,
+    },
+    arrowSelf: {
+      type: String,
+      required: true,
+    },
+    arrowOffset: {
+      type: Array,
+      required: true,
+    },
+    arrowStyle: {
+      type: String,
+      required: true,
+    },
   },
 };
 </script>
 
-<style lang="sass">
-.s-tooltip
-  padding: 6px 20px
-  border-radius: 6px
-  >div
-    line-height: 22px
-    font-size: 14px
-    font-weight: 500
+<style lang="scss">
+.s-tooltip-btn {
+  padding: 0;
+}
+.s-tooltip {
+  padding: 6px 20px;
+  border-radius: 4px;
+  font: {
+    size: 14px;
+    weight: 500;
+  }
+}
+.tooltip-triangle {
+  background: none !important;
+  padding: 0;
+  width: 0;
+  height: 0;
+}
+.bottom {
+  border: {
+    top: 0px solid transparent;
+    bottom: 12px solid $indigo-4;
+    left: 8px solid transparent;
+    right: 8px solid transparent;
+  }
+}
+.top {
+  border: {
+    top: 12px solid $indigo-4;
+    bottom: 0px solid transparent;
+    left: 8px solid transparent;
+    right: 8px solid transparent;
+  }
+}
+.left {
+  border: {
+    top: 8px solid transparent;
+    bottom: 8px solid transparent;
+    left: 12px solid $indigo-4;
+    right: 0px solid transparent;
+  }
+}
+.right {
+  border: {
+    top: 8px solid transparent;
+    bottom: 8px solid transparent;
+    left: 0px solid transparent;
+    right: 12px solid $indigo-4;
+  }
+}
+.no-hover {
+  &.q-focusable:focus > .q-focus-helper,
+  &.q-manual-focusable--focused > .q-focus-helper,
+  &.q-hoverable:hover > .q-focus-helper {
+    background: none !important;
+    opacity: 0 !important;
+    &::before,
+    &::after {
+      opacity: 0 !important;
+      background: none !important;
+    }
+  }
+}
 </style>
--- src/pages/Tooltip.vue
+++ src/pages/Tooltip.vue
@@ -1,30 +1,84 @@
 <template>
   <div class="flex justify-center q-mt-xl">
-    <q-btn
-      no-caps
-      no-wrap
-      dense
-      unelevated
-      outline
-      label="hover me"
-      color="indigo-4"
-      :ripple="false"
-      style="padding: 5px 12px; margin-top: 100px"
+    <s-tooltip
+      :boxAnchor="'bottom middle'"
+      :boxSelf="'top middle'"
+      :boxOffset="[15, 15]"
+      :arrowAnchor="'bottom middle'"
+      :arrowSelf="'center middle'"
+      :arrowStyle="'bottom'"
+      :icon="'navigation'"
+      :arrowOffset="[10, 10]"
+      noHover
+      v-model="toolOpen"
+      @click="toolOpen = !toolOpen"
     >
-      <s-tooltip anchor="top middle" self="center middle" :offset="[0, 50]">
-        <div class="q-mb-xs text-center">이것은 툴팁입니다.</div>
-        <div class="q-mb-xs text-center">마우스를 대면 나타납니다.</div>
-      </s-tooltip>
-    </q-btn>
+      <div class="text-center">이것은 툴팁입니다.</div>
+      <div class="text-center">마우스를 대면 나타납니다.</div>
+    </s-tooltip>
+  </div>
+  <div class="flex justify-center q-mt-xl">
+    <s-tooltip
+      :boxAnchor="'top middle'"
+      :boxSelf="'bottom middle'"
+      :boxOffset="[15, 15]"
+      :arrowAnchor="'top middle'"
+      :arrowSelf="'center middle'"
+      :arrowStyle="'top'"
+      :arrowOffset="[10, 10]"
+      label="Hover Me"
+      noHover
+    >
+      <div class="text-center">이것은 툴팁입니다.</div>
+      <div class="text-center">마우스를 대면 나타납니다.</div>
+    </s-tooltip>
+  </div>
+  <div class="flex justify-center q-mt-xl">
+    <s-tooltip
+      :boxAnchor="'center start'"
+      :boxSelf="'center right'"
+      :boxOffset="[15, 15]"
+      :arrowAnchor="'center left'"
+      :arrowSelf="'center middle'"
+      :arrowStyle="'left'"
+      :arrowOffset="[10, 10]"
+      :icon="'shopping_cart'"
+      noHover
+    >
+      <div class="text-center">이것은 툴팁입니다.</div>
+      <div class="text-center">마우스를 대면 나타납니다.</div>
+    </s-tooltip>
+  </div>
+  <div class="flex justify-center q-mt-xl">
+    <s-tooltip
+      :boxAnchor="'center end'"
+      :boxSelf="'center start'"
+      :boxOffset="[15, 15]"
+      :arrowAnchor="'center right'"
+      :arrowSelf="'center middle'"
+      :arrowStyle="'right'"
+      :arrowOffset="[10, 10]"
+      :icon="'shopping_cart'"
+      noHover
+    >
+      <div class="text-center">이것은 툴팁입니다.</div>
+      <div class="text-center">마우스를 대면 나타납니다.</div>
+    </s-tooltip>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 import STooltip from '../components/STooltip.vue';
 
 export default {
   components: {
     STooltip,
+  },
+  setup() {
+    return {
+      toolOpen: ref(false),
+    };
   },
 };
 </script>
